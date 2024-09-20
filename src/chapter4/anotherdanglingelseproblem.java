325833f8d945901ad2c4ac367449507ed11fe999
thefevo <thefevotechnologies@gmail.com>
2024-09-20 10:08:29 -0700

    anotherdanglingelseproblem

---
+++ src/chapter4/anotherdanglingelseproblem.java
@@ -0,0 +1,19 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class anotherdanglingelseproblem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int X=0;
+        int Y=0;
+        if (Y==8);
+        if (X==5){
+            System.out.println("@@@@@");
+        }else{
+            System.out.println("#####");
+            System.out.println("$$$$$");
+            System.out.println("&&&&&");
+        }
+    }
+}
