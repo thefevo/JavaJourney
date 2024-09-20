f8a50a74547c8b07dd19c5b00686d3c21fd2a1fa
thefevo <thefevotechnologies@gmail.com>
2024-09-20 09:16:29 -0700

    anootherdanglingstatements1

--- src/chapter4/anootherdanglingstatements1.java
+++ src/chapter4/anootherdanglingstatements1.java
@@ -8,8 +8,8 @@ public class anootherdanglingstatements1 {
         int X =0;
         int Y = 0;
 
-        if (X < 9 );
-        if (Y > 11) {
+        if (X < 11 );
+        if (Y > 9) {
             System.out.println("*****");
         }else{
             System.out.println("#####");
