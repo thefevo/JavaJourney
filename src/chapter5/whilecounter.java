5dd7ceaf36ba9d88acaba93e79306f60f983756d
thefevo <thefevotechnologies@gmail.com>
2024-09-24 13:39:57 -0700

    whilecounter

--- src/chapter5/whilecounter.java
+++ src/chapter5/whilecounter.java
@@ -1,4 +1,18 @@
 package chapter5;
 
+import java.util.Scanner;
+
 public class whilecounter {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        int counter=1;
+
+        while (counter<=0){
+            System.out.printf("%d ",counter);
+
+            ++counter;
+        }
+        System.out.println();
+    }
 }
