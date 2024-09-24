645257f61ebc17bf630c70d9f6410dc8427e8511
thefevo <thefevotechnologies@gmail.com>
2024-09-24 15:29:48 -0700

    sum

--- src/chapter5/sum.java
+++ src/chapter5/sum.java
@@ -4,5 +4,11 @@ public class sum {
     public static void main(String[] args) {
         int total=0;
 
+        for (int number=2;number<=20;number+=2){
+            total +=number;
+        }
+
+        System.out.printf("sum is %d%n",total);
+
     }
 }
