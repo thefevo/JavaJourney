49c85bbdbc3424c2c0e05078231bc93c2b78944f
thefevo <thefevotechnologies@gmail.com>
2024-09-25 13:51:16 -0700

    breaktest

---
+++ src/chapter5/breaktest.java
@@ -0,0 +1,18 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class breaktest {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+
+        int count;
+        for (count=1;count<=10;count++){
+            if (count==5){
+                break;
+            }
+           // System.out.printf("%d",count);
+
+        }
+    }
+}
