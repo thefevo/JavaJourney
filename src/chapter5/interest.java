bb66c5885a23e2fa2f56cbddb420205f238e73a9
thefevo <thefevotechnologies@gmail.com>
2024-09-24 17:03:06 -0700

    interest

--- src/chapter5/interest.java
+++ src/chapter5/interest.java
@@ -9,8 +9,13 @@ public class interest {
         double rate=0.05;
 
 
-        System.out.printf("%s%20s%n","year","Amount on deposit");
+        System.out.printf("%s%20s%n",  "year",   "Amount on deposit");
 
+        for (int year=1;year<=10;++year){
 
+            double amount = principal * Math.pow(1.0+ rate,year);
+
+            System.out.printf("%4d%,20.2f%n",year,amount);
+        }
     }
 }
