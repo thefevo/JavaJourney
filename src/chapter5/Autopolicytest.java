0eee588e3845d6640af8a47ada22d679a3c21565
thefevo <thefevotechnologies@gmail.com>
2024-09-25 12:45:36 -0700

    Autopolicytest

--- src/chapter5/Autopolicytest.java
+++ src/chapter5/Autopolicytest.java
@@ -2,6 +2,6 @@ package chapter5;
 
 public class Autopolicytest {
     public static void main(String[] args) {
-        //Autopolicy policy1=new Autopolicy(11111111,"Toyota camry","NJ");
+        Autopolicy policy1=new Autopolicy(11111111,"Toyota camry","NJ");
     }
 }
