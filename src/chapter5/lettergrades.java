6560c4ce7ef215c1cbc6c7e2f551d08a3644a129
thefevo <thefevotechnologies@gmail.com>
2024-09-25 09:33:34 -0700

    lettergrades

---
+++ src/chapter5/lettergrades.java
@@ -0,0 +1,14 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class lettergrades {
+    public static void main(String[] args) {
+
+        int total=0;
+        int gradecounter=0;
+        Scanner input=new Scanner(System.in);
+
+
+    }
+}
