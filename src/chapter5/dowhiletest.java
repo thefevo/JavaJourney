8585d85632f08589a1f2534a21aba4d97960476d
thefevo <thefevotechnologies@gmail.com>
2024-09-25 09:22:25 -0700

    dowhiletest

---
+++ src/chapter5/dowhiletest.java
@@ -0,0 +1,19 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class dowhiletest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int counter = 1;
+
+       // do {
+           // System.out.printf("%d ", counter);
+           // ++counter;
+       // } while (counter <= 10) ;
+       // System.out.println();
+    }
+
+}
+
