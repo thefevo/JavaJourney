1897c96db510e523fec79d470657c847c45c3da1
thefevo <thefevotechnologies@gmail.com>
2024-09-26 10:34:28 -0700

    logicaloperators

--- src/chapter5/logicaloperators.java
+++ src/chapter5/logicaloperators.java
@@ -5,5 +5,7 @@ public class logicaloperators {
         System.out.printf("%s%n%s: %b%n%s: %b%n%s: %b%n%s: %b%n%s:","conditional AND (&&)",
                 "false && false",("false && false"),"false && true",("false && true"),
                 "true && false",("true && false"),"true && true",("true && true"));
+
+        System.out.printf("%s%n%s: %b%n%s: %b%n%s: %b%n%s: %b%n%n");
     }
 }
