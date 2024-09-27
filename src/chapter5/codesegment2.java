f09985b24a6c6362eab6aaec3357ecbfb87a7544
thefevo <thefevotechnologies@gmail.com>
2024-09-27 15:43:27 -0700

    codesegment2

--- src/chapter5/codesegment2.java
+++ src/chapter5/codesegment2.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class codesegment2 {
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
+        for (int k = (int) 0.1; k !=1.0; k += 0.1){
+            System.out.println(k);
+        }
     }
 }
