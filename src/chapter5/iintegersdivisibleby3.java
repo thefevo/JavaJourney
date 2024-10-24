0e14c14542fb6ce94b879c90cc1c79c45311053b
thefevo <thefevotechnologies@gmail.com>
2024-10-24 13:24:45 -0700

    iintegersdivisibleby3

--- src/chapter5/iintegersdivisibleby3.java
+++ src/chapter5/iintegersdivisibleby3.java
@@ -12,7 +12,7 @@ public class iintegersdivisibleby3 {
         // Loop through numbers from 1 to 30
         for (int i = 1; i <= 30; i++) {
             // Check if the number is divisible by 3
-           // if (i % 3 == 0) {
+            if (i % 3 == 0) {
                // sum += i; // Add to sum if divisible by 3
            // }
         //}
